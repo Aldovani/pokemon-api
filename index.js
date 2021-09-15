caa783d981a52f69f852efc37ce26f0fe7017b4b
Aldovani <aldovane.etec@gmail.com>
2021-09-15 19:55:51 -0300

    Adição categoria

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const pokemonDB = require("./data/pokemonDb.json");
 const app = express();
 
 app.get("/", (req, res) => {
+  res.send("👍");
+});
+
+app.get("/pokemon", (req, res) => {
   res.type("json");
   res.send(pokemonDB);
 });
@@ -10,33 +14,69 @@ app.get("/", (req, res) => {
 app.get("/pokemon/:id", (req, res) => {
   res.type("json");
 
-  const result = pokemonDB.pokemons.filter((e) => {
-    return e.id == req.params.id;
-  });
+  if (!isNaN(req.params.id)) {
+    const result = pokemonDB.pokemons.filter((e) => {
+      return e.id == req.params.id;
+    });
 
-  if (result.length === 0) {
-    res.send({"Error":"Pokemon not found"});    
+    if (result.length === 0) {
+      res.send({ Error: "Pokemon not found" });
+    }
+    return res.send(result);
+  } else {
+    const result = pokemonDB.pokemons.filter((e) => {
+      return e.name == req.params.id;
+    });
+
+    if (result.length === 0) {
+      res.send({ Error: "Pokemon not found" });
+    }
+    return res.send(result);
   }
-
-  res.send(result);
 });
-
 
 app.get("/pokemon/raridade/:tipo", (req, res) => {
   res.type("json");
 
-  const result = pokemonDB.pokemons.filter((e) => {
+  const raridade = pokemonDB.pokemons.filter((e) => {
     return e.raridade == req.params.tipo;
   });
 
-  if (result.length === 0) {
-    res.send({"Error":"type not found"});    
+  if (raridade.length === 0) {
+    res.send({ Error: "type not found" });
   }
 
-  res.send(result);
+  return res.send(result);
 });
 
+app.get("/pokemon/raridade/:tipo/:idName", (req, res) => {
+  res.type("json");
 
-app.listen(process.env.PORT||8080, () => {
+  const result = pokemonDB.pokemons.filter(
+    (e) => e.raridade == req.params.tipo
+  );
+
+  if (result.length === 0) {
+    res.send({ Error: "type not found" });
+  }
+
+  if (!isNaN(req.params.idName)) {
+    const result2 = result.filter((e) => e.id == req.params.idName);
+    if (result2.length === 0) {
+      res.send({ Error: "pokemon not found" });
+    }
+    return res.send(result2);
+  } else {
+    const result2 = result.filter((e) => e.name == req.params.idName);
+    if (result2.length === 0) {
+      res.send({ Error: "pokemon not found" });
+    }
+    return res.send(result2);
+  }
+
+  return res.send(result);
+});
+
+app.listen(process.env.PORT || 8080, () => {
   console.log("server running on port 8080");
 });
